7ae773727d106d00f8af7be70cc594be7805602b
jonas breen <jonasebreen@gmail.com>
2024-12-01 01:52:26 -0500

    added bin option

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,17 @@ struct Cli {
 
 #[derive(Debug, Subcommand)]
 enum Commands {
-    Hex(Hex)
+    Hex(Hex),
+    Bin(Bin),
 }
 
 #[derive(Debug, Args)]
 struct Hex {
+    values: Vec<String>
+}
+
+#[derive(Debug, Args)]
+struct Bin {
     values: Vec<String>
 }
 
@@ -33,12 +39,33 @@ fn main() {
         Commands::Hex(scmd) => {
             let mut input: String = scmd.values.join("");
             input = input.trim_start_matches("0x").to_string();
-            let output = group(input.chars()).map(|x| {
-                let mut s = String::from(x.0);
-                s.push(x.1);
-                s
+            let output = group(2, input.chars()).map(|x| {
+                String::from(x.into_iter().fold(String::new(), |mut acc, x| {
+                    acc.push(x);
+                    acc
+                }))
             }).map(|x| {
                     i16::from_str_radix(&x, 16).unwrap()
+                })
+                .map(|x| x as u8 as char)
+                .fold(String::new(), |mut str, x| {
+                    str.push(x);
+                    str
+                });
+
+            println!("{}", output);
+        }
+        Commands::Bin(scmd) => {
+            let mut input: String = scmd.values.join("");
+            input = input.trim_start_matches("0x").to_string();
+            let output = group(8, input.chars()).map(|x| {
+                String::from(x.into_iter().fold(String::new(), |mut acc, x| {
+                    acc.push(x);
+                    acc
+                }))
+            })
+                .map(|x| {
+                    i16::from_str_radix(&x, 2).unwrap()
                 })
                 .map(|x| x as u8 as char)
                 .fold(String::new(), |mut str, x| {
@@ -54,15 +81,23 @@ fn main() {
 #[derive(Debug)]
 struct GroupIter<I> {
     iter: I,
+    size: u32,
 }
 
 impl<I> Iterator for GroupIter<I> where I: Iterator {
-    type Item = (I::Item, I::Item);
+    type Item = Vec<I::Item>;
     fn next(&mut self) -> Option<Self::Item> {
-        Some((self.iter.next()?, self.iter.next()?))
+        let mut out = Vec::new();
+        for _ in 1..=self.size {
+            out.push(self.iter.next()?);
+        }
+        Some(out)
     }
 }
 
-fn group<I>(iter: I) -> GroupIter<I> where I: Iterator {
-    GroupIter { iter }
+fn group<I>(size: u32, iter: I) -> GroupIter<I> where I: Iterator {
+    GroupIter {
+        iter,
+        size,
+    }
 }
